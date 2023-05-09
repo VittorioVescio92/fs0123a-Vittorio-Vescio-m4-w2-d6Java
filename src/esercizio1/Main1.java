a886bd480c274f5e79c6f44a7f94d12f30663144
VittorioVescio92 <123975369+VittorioVescio92@users.noreply.github.com>
2023-05-09 08:38:56 +0200

    codice Main rivisto e ridotto

--- src/esercizio1/Main1.java
+++ src/esercizio1/Main1.java
@@ -20,83 +20,102 @@ public class Main1 {
 			System.out.println(Arrays.toString(randomInt));
 			System.out.println("Scrivi quale posizione dell'array intendi modificare");
 			int indexSelected = scan.nextInt();
-			if (indexSelected > 0) {
-				switch (indexSelected) {
-				case 1:
-					System.out.println("Che valore vuoi inserire?");
-					try {
-						int indexValue = scan.nextInt();
-						if (indexValue < 0 || indexValue > 10) {
-							throw new From1To10Exception("Devi inserire un valore compreso tra 1 e 10");
-						} else {
-							randomInt[0] = indexValue;
-						}
-					} catch (From1To10Exception e) {
-						System.out.println(e.getMessage());
+			if (indexSelected > 0 && indexSelected <= 5) {
+				System.out.println("Che valore vuoi inserire?");
+				try {
+					int indexValue = scan.nextInt();
+					if (indexValue < 0 || indexValue > 10) {
+						throw new From1To10Exception("Devi inserire un valore compreso tra 1 e 10");
+					} else {
+						randomInt[indexSelected - 1] = indexValue;
 					}
-					break;
-				case 2:
-					System.out.println("Che valore vuoi inserire?");
-					try {
-						int indexValue = scan.nextInt();
-						if (indexValue < 0 || indexValue > 10) {
-							throw new From1To10Exception("Devi inserire un valore compreso tra 1 e 10");
-						} else {
-							randomInt[1] = indexValue;
-						}
-					} catch (From1To10Exception e) {
-						System.out.println(e.getMessage());
-					}
-					break;
-				case 3:
-					System.out.println("Che valore vuoi inserire?");
-					try {
-						int indexValue = scan.nextInt();
-						if (indexValue < 0 || indexValue > 10) {
-							throw new From1To10Exception("Devi inserire un valore compreso tra 1 e 10");
-						} else {
-							randomInt[2] = indexValue;
-						}
-					} catch (From1To10Exception e) {
-						System.out.println(e.getMessage());
-					}
-					break;
-				case 4:
-					System.out.println("Che valore vuoi inserire?");
-					try {
-						int indexValue = scan.nextInt();
-						if (indexValue < 0 || indexValue > 10) {
-							throw new From1To10Exception("Devi inserire un valore compreso tra 1 e 10");
-						} else {
-							randomInt[3] = indexValue;
-						}
-					} catch (From1To10Exception e) {
-						System.out.println(e.getMessage());
-					}
-					break;
-				case 5:
-					System.out.println("Che valore vuoi inserire?");
-					try {
-						int indexValue = scan.nextInt();
-						if (indexValue < 0 || indexValue > 10) {
-							throw new From1To10Exception("Devi inserire un valore compreso tra 1 e 10");
-						} else {
-							randomInt[4] = indexValue;
-						}
-					} catch (From1To10Exception e) {
-						System.out.println(e.getMessage());
-					}
-					break;
-				default:
-//					quit = 0;
-					System.out.println("Inserisci un valore da 1 a 5!");
-					break;
+				} catch (From1To10Exception e) {
+					System.out.println(e.getMessage());
 				}
+			} else if (indexSelected != 0) {
+				System.out.println("Inserisci un valore da 1 a 5!");
 			} else {
 				System.out.println("Applicazione in chiusura, grazie per averla utilizzata!");
 			}
+
 		}
 		System.out.println("prova");
 		scan.close();
 	}
 }
+
+//if (indexSelected > 0) {
+//switch (indexSelected) {
+//case 1:
+//	System.out.println("Che valore vuoi inserire?");
+//	try {
+//		int indexValue = scan.nextInt();
+//		if (indexValue < 0 || indexValue > 10) {
+//			throw new From1To10Exception("Devi inserire un valore compreso tra 1 e 10");
+//		} else {
+//			randomInt[0] = indexValue;
+//		}
+//	} catch (From1To10Exception e) {
+//		System.out.println(e.getMessage());
+//	}
+//	break;
+//case 2:
+//	System.out.println("Che valore vuoi inserire?");
+//	try {
+//		int indexValue = scan.nextInt();
+//		if (indexValue < 0 || indexValue > 10) {
+//			throw new From1To10Exception("Devi inserire un valore compreso tra 1 e 10");
+//		} else {
+//			randomInt[1] = indexValue;
+//		}
+//	} catch (From1To10Exception e) {
+//		System.out.println(e.getMessage());
+//	}
+//	break;
+//case 3:
+//	System.out.println("Che valore vuoi inserire?");
+//	try {
+//		int indexValue = scan.nextInt();
+//		if (indexValue < 0 || indexValue > 10) {
+//			throw new From1To10Exception("Devi inserire un valore compreso tra 1 e 10");
+//		} else {
+//			randomInt[2] = indexValue;
+//		}
+//	} catch (From1To10Exception e) {
+//		System.out.println(e.getMessage());
+//	}
+//	break;
+//case 4:
+//	System.out.println("Che valore vuoi inserire?");
+//	try {
+//		int indexValue = scan.nextInt();
+//		if (indexValue < 0 || indexValue > 10) {
+//			throw new From1To10Exception("Devi inserire un valore compreso tra 1 e 10");
+//		} else {
+//			randomInt[3] = indexValue;
+//		}
+//	} catch (From1To10Exception e) {
+//		System.out.println(e.getMessage());
+//	}
+//	break;
+//case 5:
+//	System.out.println("Che valore vuoi inserire?");
+//	try {
+//		int indexValue = scan.nextInt();
+//		if (indexValue < 0 || indexValue > 10) {
+//			throw new From1To10Exception("Devi inserire un valore compreso tra 1 e 10");
+//		} else {
+//			randomInt[4] = indexValue;
+//		}
+//	} catch (From1To10Exception e) {
+//		System.out.println(e.getMessage());
+//	}
+//	break;
+//default:
+////	quit = 0;
+//	System.out.println("Inserisci un valore da 1 a 5!");
+//	break;
+//}
+//} else {
+//System.out.println("Applicazione in chiusura, grazie per averla utilizzata!");
+//}
